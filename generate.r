a8e6aad6890f7bf80f7906a8c03d9e47cbb5a213
Konrad Rudolph <konrad.rudolph@gmail.com>
2020-04-19 00:21:57 +0100

    Improve names

--- generate.r
+++ generate.r
@@ -1,12 +1,12 @@
 # After <https://twitter.com/georgemsavva/status/1250914089028763649>.
 
-f = function (t, sign, rotations) {
-    series = 0 : rotations
+f = function (t, sign, circles) {
+    series = 0 : circles
     rot = function (t) sum(exp(1i * pi * t * (-5) ^ series) / ((sign * 2.5) ^ series))
     vapply(t, rot, complex(1L))
 }
 
-draw_handles = function (pts) {
+draw_radii = function (pts) {
     pts = c(0, pts)
     x = vapply(pts, Re, numeric(1L))
     y = vapply(pts, Im, numeric(1L))
@@ -14,7 +14,7 @@ draw_handles = function (pts) {
     points(x, y, pch = 20, col = 'lightgray')
 }
 
-plot_rosetta = function (ts, z1, z2, up_to, rotations) {
+plot_rosetta = function (ts, z1, z2, up_to, circles) {
     is = seq_len(up_to)
     z1 = z1[is]
     z2 = z2[is]
@@ -26,11 +26,11 @@ plot_rosetta = function (ts, z1, z2, up_to, rotations) {
     lines(Re(z1), Im(z1), col = '#E36209')
     lines(Re(z2), Im(z2), col = '#46AFF8')
 
-    pts1 = vapply(0 : rotations, f, complex(1L), t = ts[up_to], sign = 1)
-    pts2 =  vapply(0 : rotations, f, complex(1L), t = ts[up_to], sign = -1)
+    pts1 = vapply(0 : circles, f, complex(1L), t = ts[up_to], sign = 1)
+    pts2 =  vapply(0 : circles, f, complex(1L), t = ts[up_to], sign = -1)
 
-    draw_handles(pts1)
-    draw_handles(pts2)
+    draw_radii(pts1)
+    draw_radii(pts2)
 }
 
 # Use cosine interpolation to smooth the beginning and end of the animation.
@@ -45,10 +45,10 @@ cosine_interpolate = function (t, to) {
 figure_points = 100000L
 frames = 1000L
 
-rotations = 4L
+circles = 4L
 ts = seq(0, 2, length.out = figure_points)
-z1data = f(ts, 1, rotations)
-z2data = f(ts, -1, rotations)
+z1data = f(ts, 1, circles)
+z2data = f(ts, -1, circles)
 
 dir.create('img', showWarnings = FALSE)
 
@@ -57,7 +57,7 @@ for (frame in seq(frames)) {
     idx = cosine_interpolate(t, length(ts))
     cat(sprintf('\rt = %d (%.1f%%)', idx, idx / length(ts) * 100))
     png(sprintf('img/rosetta_%03d.png', frame))
-    plot_rosetta(ts, z1data, z2data, up_to = idx, rotations = rotations)
+    plot_rosetta(ts, z1data, z2data, up_to = idx, circles = circles)
     dev.off()
 }
 
